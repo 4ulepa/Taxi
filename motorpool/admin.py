94339f2af705cf48c603ac36ea7bd6f85bddfc0f
Xnezer <xnezerdev@yandex.ru>
2021-12-04 01:21:42 +0300

    Added Auto model

--- motorpool/admin.py
+++ motorpool/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Brand
+from .models import Brand, Auto
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
     pass
--- motorpool/models.py
+++ motorpool/models.py
@@ -11,3 +11,26 @@ class Brand(models.Model):
         verbose_name = 'Бренд'
         verbose_name_plural = 'Бренды'
         ordering = ['title']  # Сортировка по названию
+
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'Эконом'),
+        (AUTO_CLASS_COMFORT, 'Комфорт'),
+        (AUTO_CLASS_BUSINESS, 'Бизнес'),
+    )
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
